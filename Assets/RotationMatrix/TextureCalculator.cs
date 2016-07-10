02a8d080021644b29bd78d92626983d24e6ed15e
Lauri Miettinen <lauri.miettinen90@gmail.com>
2016-07-10 11:51:21 +0300

    Fixed all problems with texture rotation.

---
+++ Assets/RotationMatrix/TextureCalculator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureCalculator {
+
+	static Vector2[] CalculateMatrix(float angleDeg) {
+		//Vector2[] rotationMatrix = { new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)), new Vector2(-1f * Mathf.Sin(angle), Mathf.Cos(angle)) };
+		float angleRad = Mathf.Deg2Rad * angleDeg;
+
+		Vector2[] rotationMatrix = { new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad)), new Vector2(-1f * Mathf.Sin(angleRad), Mathf.Cos(angleRad)) };
+		return rotationMatrix;
+	}
+
+	public static Vector2[] GetRotatedUv(float angle, Vector2[] originalUv) {
+
+		Vector2[] rotationMatrix = TextureCalculator.CalculateMatrix(-angle);
+
+		Debug.Log(originalUv.Length);
+
+		Vector2[] rotatedUv = new Vector2[originalUv.Length];
+
+		for (int i = 0; i < originalUv.Length; i++) {
+			rotatedUv[i].x = Vector2.Dot(rotationMatrix[0], originalUv[i]);
+			rotatedUv[i].y = Vector2.Dot(rotationMatrix[1], originalUv[i]);
+		}
+
+		return rotatedUv;
+	}
+}
---
+++ Assets/RotationMatrix/TextureRotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureRotator : MonoBehaviour {
+
+	public int givenAngle = 25;
+
+	public GameObject rotatedObject;
+
+	void Start() {
+		RotateTexture(rotatedObject, givenAngle);
+    }
+
+	void RotateTexture(GameObject _gameobject ,float _angle) {
+		MeshFilter meshFilter = _gameobject.GetComponent<MeshFilter>();
+		Mesh m_Mesh = meshFilter.mesh;
+
+		Vector2[] vectors = TextureCalculator.GetRotatedUv(givenAngle, m_Mesh.uv);
+		m_Mesh.uv = vectors;
+	}
+}
