71abf403ebb5d750921b94d95ecf8c15ebedf9fa
Lauri Miettinen <lauri.miettinen90@gmail.com>
2016-07-10 11:07:40 +0300

    Figure out why Unity crashes upon entering play mode >_<

--- Assets/RotationMatrix/BetterRotationMatrix.cs
+++ Assets/RotationMatrix/BetterRotationMatrix.cs
@@ -8,28 +8,41 @@ public class BetterRotationMatrix : MonoBehaviour {
 
 	// Use this for initialization
 	void Start() {
-		MeshFilter meshFilter = GetComponent<MeshFilter>();
-		Mesh m_Mesh = meshFilter.mesh;
 
-		Vector2[] vectors = GetRotatedUv(givenAngle, m_Mesh.uv);
-		m_Mesh.uv = vectors;
+		RotateUv(givenAngle, this.gameObject.GetComponent<Mesh>());
 
-		Destroy(this);
 	}
 
-	Vector2[] GetRotatedUv(float angle, Vector2[] originalUv) {
+	public static Vector2[] CalculateMatrix(float _angleDeg) {
+		//Vector2[] rotationMatrix = { new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)), new Vector2(-1f * Mathf.Sin(angle), Mathf.Cos(angle)) };
+		float angleRad = Mathf.Deg2Rad * _angleDeg;
 
-		Vector2[] rotationMatrix = MatrixRotator.CalculateMatrix(-angle);
+		Vector2[] rotationMatrix = { new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad)), new Vector2(-1f * Mathf.Sin(angleRad), Mathf.Cos(angleRad)) };
+		return rotationMatrix;
+	}
 
-		Debug.Log(originalUv.Length);
+	public static Vector2[] GetRotatedUv(float _angle, Vector2[] _originalUv) {
 
-		Vector2[] rotatedUv = new Vector2[originalUv.Length];
+		Vector2[] rotationMatrix = CalculateMatrix(_angle);
 
-		for (int i = 0; i < originalUv.Length; i++) {
-			rotatedUv[i].x = Vector2.Dot(rotationMatrix[0], originalUv[i]);
-			rotatedUv[i].y = Vector2.Dot(rotationMatrix[1], originalUv[i]);
+		Debug.Log(_originalUv.Length);
+
+		Vector2[] rotatedUv = new Vector2[_originalUv.Length];
+
+		for (int i = 0; i < _originalUv.Length; i++) {
+			rotatedUv[i].x = Vector2.Dot(rotationMatrix[0], _originalUv[i]);
+			rotatedUv[i].y = Vector2.Dot(rotationMatrix[1], _originalUv[i]);
 		}
 
 		return rotatedUv;
+	}
+
+	public static void RotateUv(float _angle, Mesh _mesh) {
+
+		Vector2[] originalUv = _mesh.uv;
+
+		Vector2[] rotatedUv = GetRotatedUv(-_angle, originalUv);
+
+		_mesh.uv = rotatedUv;
 	}
 }
--- Assets/RotationMatrix/MatrixRotator.cs
+++ Assets/RotationMatrix/MatrixRotator.cs
@@ -1,13 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MatrixRotator : MonoBehaviour {
+public class MatrixRotator :MonoBehaviour{
 
-	public static Vector2[] CalculateMatrix(float angleDeg) {
+	public static Vector2[] CalculateMatrix(float _angleDeg) {
 		//Vector2[] rotationMatrix = { new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)), new Vector2(-1f * Mathf.Sin(angle), Mathf.Cos(angle)) };
-		float angleRad = Mathf.Deg2Rad * angleDeg;
+		float angleRad = Mathf.Deg2Rad * _angleDeg;
 
 		Vector2[] rotationMatrix = { new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad)), new Vector2(-1f * Mathf.Sin(angleRad), Mathf.Cos(angleRad)) };
 		return rotationMatrix;
 	}
+
+	public static Vector2[] GetRotatedUv(float _angle, Vector2[] _originalUv) {
+
+		Vector2[] rotationMatrix = MatrixRotator.CalculateMatrix(_angle);
+
+		Debug.Log(_originalUv.Length);
+
+		Vector2[] rotatedUv = new Vector2[_originalUv.Length];
+
+		for (int i = 0; i < _originalUv.Length; i++) {
+			rotatedUv[i].x = Vector2.Dot(rotationMatrix[0], _originalUv[i]);
+			rotatedUv[i].y = Vector2.Dot(rotationMatrix[1], _originalUv[i]);
+		}
+
+		return rotatedUv;
+	}
+
+	public static void RotateUv(float _angle, Mesh _mesh) {
+
+
+		Vector2[] originalUv = _mesh.uv;
+
+		Vector2[] rotatedUv = MatrixRotator.GetRotatedUv(-_angle, originalUv);
+
+		_mesh.uv = rotatedUv;
+	}
+	/*
+	public static void RotateUv(float _angle, GameObject _gameObject) {
+
+		Mesh mesh = _gameObject.GetComponent<Mesh>();
+
+		Vector2[] originalUv = mesh.uv;
+
+		Vector2[] rotatedUv = MatrixRotator.GetRotatedUv(_angle, originalUv);
+
+		mesh.uv = rotatedUv;
+	}*/
 }
