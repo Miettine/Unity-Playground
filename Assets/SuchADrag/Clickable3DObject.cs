3584a1b38b9fb1e770b9ba3e1125e0ed9a921488
Lauri Miettinen <lauri@groupbuilder.com>
2016-07-12 16:02:01 +0300

    Finished dragging mini project.

---
+++ Assets/SuchADrag/Clickable3DObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clickable3DObject : MonoBehaviour {
+
+	void OnMouseDown() {
+		Debug.Log (gameObject.name + ": OnMouseDown");
+	}
+
+	void OnMouseUp() {
+		Debug.Log(gameObject.name + ": OnMouseUp");
+	}
+
+	void OnMouseDrag() {
+		Debug.Log(gameObject.name + ": Drag");
+	}
+}
