46f2e0aa19f5670a33a23bb527cbab4c35097159
Lauri Miettinen <lauri@groupbuilder.com>
2016-07-12 15:10:53 +0300

    Assets to the event system project.

---
+++ Assets/SuchADrag/DragHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DragHandler : MonoBehaviour, IDragHandler,IBeginDragHandler, IEndDragHandler,IPointerClickHandler {
+
+	public void OnBeginDrag(PointerEventData _data) {
+		Debug.Log("Started dragging " + gameObject.name + "!");
+	}
+
+	public void OnDrag(PointerEventData _data) {
+		Debug.Log("You are dragging "+gameObject.name+"!");
+	}
+
+	public void OnEndDrag(PointerEventData _data) {
+		Debug.Log("Finished dragging " + gameObject.name + ".");
+	}
+
+	public void OnPointerClick(PointerEventData _data) {
+		Debug.Log("You clicked " + gameObject.name + "!");
+	}
+
+}
