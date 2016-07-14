5163f26027b267268af20f7f9fce780942fed6e0
Lauri Miettinen <lauri@groupbuilder.com>
2016-07-14 15:19:08 +0300

    Conclusion: Need to make a system that stores data in between builds.

---
+++ Assets/BigDataPlayerPrefs/BigPlayerPrefs.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BigPlayerPrefs : MonoBehaviour {
+
+	public InputField inputField;
+
+	void Start() {
+		Debug.Log(Application.persistentDataPath);
+	}
+
+	public void Save_OnClick() {
+		PlayerPrefs.SetString("big-data", inputField.text);
+	}
+
+	public void Load_OnClick() {
+		inputField.text = PlayerPrefs.GetString("big-data", "Nothing saved to playerprefs!");
+	}
+
+	public void Delete_OnClick() {
+		PlayerPrefs.DeleteKey("big-data");
+	}
+}
