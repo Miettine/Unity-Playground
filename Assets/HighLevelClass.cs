e8a70163d5ad8ab8f3cbffc347893cd9fb94cf22
Lauri Miettinen <lauri.miettinen90@gmail.com>
2016-08-22 08:49:39 +0300

    Began work

---
+++ Assets/HighLevelClass.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighLevelClass : MonoBehaviour {
+
+	void OnListValueChanged(int _value) {
+		Debug.Log("HighLevelClass, OnListValueChanged:" + _value);
+	}
+}
