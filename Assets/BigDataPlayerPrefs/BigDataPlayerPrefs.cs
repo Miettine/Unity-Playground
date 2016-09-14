95c600d1def621a97059ad1908a9ec624590ee5c
Miettine <lauri.miettinen90@gmail.com>
2016-09-14 13:28:59 +0300

    Large amounts of data can be saved to playerprefs, data persists across builds as long as the builds have the same name and location.

--- Assets/BigDataPlayerPrefs/BigDataPlayerPrefs.cs
+++ Assets/BigDataPlayerPrefs/BigDataPlayerPrefs.cs
@@ -10,15 +10,18 @@ public class BigDataPlayerPrefs : MonoBehaviour {
 
 
 	public int dataPoints=30;
+
+	string version = "version3";
+
 	public void Save_OnClick() {
 
-		//Contains many lines of JSON-like text. Replaced apostrophes with dollar signs to prevent problems, made into a single string.
+		//Contains many lines of JSON text. Replaced apostrophes with dollar signs to prevent problems, made into a single string.
 		string baseData = "{$web-app$: { $servlet$: [ { $servlet-name$: $cofaxCDS$, $servlet-class$: $org.cofax.cds.CDSServlet$, $init-param$: { $configGlossary:installationAt$: $Philadelphia, PA$, $configGlossary:adminEmail$: $ksm@pobox.com$, $configGlossary:poweredBy$: $Cofax$, $configGlossary:poweredByIcon$: $/images/cofax.gif$, $configGlossary:staticPath$: $/content/static$, $templateProcessorClass$: $org.cofax.WysiwygTemplate$, $templateLoaderClass$: $org.cofax.FilesTemplateLoader$, $templatePath$: $templates$, $templateOverridePath$: $$, $defaultListTemplate$: $listTemplate.htm$, $defaultFileTemplate$: $articleTemplate.htm$, $useJSP$: false, $jspListTemplate$: $listTemplate.jsp$, $jspFileTemplate$: $articleTemplate.jsp$, $cachePackageTagsTrack$: 200, $cachePackageTagsStore$: 200, $cachePackageTagsRefresh$: 60, $cacheTemplatesTrack$: 100, $cacheTemplatesStore$: 50, $cacheTemplatesRefresh$: 15, $cachePagesTrack$: 200, $cachePagesStore$: 100, $cachePagesRefresh$: 10, $cachePagesDirtyRead$: 10, $searchEngineListTemplate$: $forSearchEnginesList.htm$, $searchEngineFileTemplate$: $forSearchEngines.htm$, $searchEngineRobotsDb$: $WEB-INF/robots.db$, $useDataStore$: true, $dataStoreClass$: $org.cofax.SqlDataStore$, $redirectionClass$: $org.cofax.SqlRedirection$, $dataStoreName$: $cofax$, $dataStoreDriver$: $com.microsoft.jdbc.sqlserver.SQLServerDriver$, $dataStoreUrl$: $jdbc:microsoft:sqlserver://LOCALHOST:1433;DatabaseName=goon$, $dataStoreUser$: $sa$, $dataStorePassword$: $dataStoreTestQuery$, $dataStoreTestQuery$: $SET NOCOUNT ON;select test='test';$, $dataStoreLogFile$: $/usr/local/tomcat/logs/datastore.log$, $dataStoreInitConns$: 10, $dataStoreMaxConns$: 100, $dataStoreConnUsageLimit$: 100, $dataStoreLogLevel$: $debug$, $maxUrlLength$: 500}}, { $servlet-name$: $cofaxEmail$, $servlet-class$: $org.cofax.cds.EmailServlet$, $init-param$: { $mailHost$: $mail1$, $mailHostOverride$: $mail2$}}, { $servlet-name$: $cofaxAdmin$, $servlet-class$: $org.cofax.cds.AdminServlet$}, { $servlet-name$: $fileServlet$, $servlet-class$: $org.cofax.cds.FileServlet$}, { $servlet-name$: $cofaxTools$, $servlet-class$: $org.cofax.cms.CofaxToolsServlet$, $init-param$: { $templatePath$: $toolstemplates/$, $log$: 1, $logLocation$: $/usr/local/tomcat/logs/CofaxTools.log$, $logMaxSize$: $$, $dataLog$: 1, $dataLogLocation$: $/usr/local/tomcat/logs/dataLog.log$, $dataLogMaxSize$: $$, $removePageCache$: $/content/admin/remove?cache=pages&id=$, $removeTemplateCache$: $/content/admin/remove?cache=templates&id=$, $fileTransferFolder$: $/usr/local/tomcat/webapps/content/fileTransferFolder$, $lookInContext$: 1, $adminGroupID$: 4, $betaServer$: true}}], $servlet-mapping$: { $cofaxCDS$: $/$, $cofaxEmail$: $/cofaxutil/aemail/*$, $cofaxAdmin$: $/admin/*$, $fileServlet$: $/static/*$, $cofaxTools$: $/tools/*$}, $taglib$: { $taglib-uri$: $cofax.tld$, $taglib-location$: $/WEB-INF/tlds/cofax.tld$}}}";
 
 		//string[] bigDataArray;
 
 		for (int i = 0; i< dataPoints; i++) {
-			string data = "data " + i + " :" + baseData;
+			string data = "data " + i + "version3" + " :" + baseData + baseData;
 			string fakeguid = "guid" + i + "-aaaaa-aaaaa-aaaaa";
 
 			PlayerPrefs.SetString(fakeguid, data);
