5b6d48f19523266266cf98bc4ecfd02fe834892b
Lauri Miettinen <lauri.miettinen90@gmail.com>
2016-10-22 15:57:47 +0300

    Statement: CoRoutines do work.

--- Assets/Singleton/Renegade.cs
+++ Assets/Singleton/Renegade.cs
@@ -4,7 +4,14 @@ using System.Collections;
 public class Renegade : Singleton<Renegade>, ICanSpeak {
 
 	public string Speak() {
+		StartCoroutine(LateComment("Long live the revolution!"));
 		return "Fight the system!";
+		
+	}
+
+	IEnumerator LateComment(string _comment) {
+		yield return new WaitForSeconds(4f);
+		Debug.Log(_comment);
 	}
 
 }
